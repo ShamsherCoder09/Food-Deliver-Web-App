bb87b4382cab2c473a7cbf29b70b23caa775cac4
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-09-18 21:59:55 +0530

    Header and Menu part is done

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite Food Here.</h2>
+            <p>Lorem ipsum dolor sit, voluptate ullam, dolore ab voluptatem, cumque reprehenderit nisi voluptas corporis eum vero a sed fugiat labore cupiditate tempora est et? Quas deserunt debitis officiis culpa tempora dignissimos voluptatem dicta sint et recusandae, sunt officia. Perferendis?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
