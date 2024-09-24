2e2aed51ae1f05e55ed2cf7aa91ea5b7bdb64a42
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-09-25 00:02:48 +0530

    responsive work is don

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 
 function AppDownload() {
   return (
-    <div className="app-download">
+    <div className="app-download" id='app-download'>
         <p>For Better Experience <br/> Tomato App</p>
         <div className="app-download-platform">
             <img src={assets.play_store} alt="" />
