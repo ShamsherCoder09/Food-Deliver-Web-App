cb95ceae27296d7ae0690cd50bb70424d3dd1dbf
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-09-27 19:41:49 +0530

    cart item is done and make the responsive

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -20,14 +20,26 @@ const StoreContextProvider = (props)=>{
 
     useEffect(()=>{
         console.log(cartItems)
-    },[cartItems])
+    },[cartItems]);
+
+    const getTotalCartAmount =()=>{
+        let totalAmount = 0;
+        for(let item in cartItems){
+            if(cartItems[item]>0){
+                let infoItem = food_list.find((product)=> product._id === item);
+            totalAmount += infoItem.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue={
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeToCart
+        removeToCart,
+        getTotalCartAmount
     }
     return (
         <StoreContext.Provider value={contextValue}>
