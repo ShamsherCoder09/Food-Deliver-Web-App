524881c666e661e3ff3e13ff790285bea7b8c25d
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-09-29 10:30:00 +0530

    creating the logic for listFood Item

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,25 +1,40 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs';
+import fs from "fs";
 
 // add food item
-const addFood = async(req, res)=>{
-    const image_filename = `${req.file.filename}`;
+const addFood = async (req, res) => {
+  const image_filename = `${req.file.filename}`;
 
-    const food = new foodModel({
-        name:req.body.name,
-        description:req.body.description,
-        price:req.body.price,
-        category:req.body.category,
-        image:image_filename
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// get food lists
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({
+      success: true,
+      message: foods,
     });
-    try {
-        await food.save();
-        res.json({success:true,message:"Food Added"})
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:"Error"});
-    }
-}
-
-export {addFood}
-
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "showing error when i getting foodlist ",
+    });
+  }
+};
+export { addFood, listFood };
