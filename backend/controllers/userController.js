b8a1bf2523433bc69fedd1bfee4012a5be1ce32c
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-10-02 15:35:44 +0530

    registerController login created successfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,9 +8,58 @@ const userLoging = async(req,res)=>{
 
 }
 
+const createToken = (id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET);
+}
+
 // user register
 const userRegister = async(req,res)=>{
+    const {name,password,email} = req.body;
+    try {
+        // Is email already exists
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({
+                success:false,
+                message:"User is already exists"
+            });
+        }
+        // validated email format and strong password;
+        if(!validator.isEmail(email)){
+            return res.json({
+                success:false,
+                message:"Email format is not correct"
+            });
+        }
+        if(password.length<8){
+            return res.json({
+                success:false,
+                message:"Password is not strong"
+            });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        });
+        const user = await newUser.save();
+        const token = createToken(user._id);
 
+        res.json({
+            success:true,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success:false,
+            message:"User doesn't register"
+        })
+        
+    }
 }
 
 export {userLoging,userRegister} ;
